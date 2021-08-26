d4541dc1013e96eb02c28b06352c41074f7eb8cf
sonnguyenbk <sonnguyenbk97@gmail.com>
2021-08-26 09:34:11 +0700

    Init

---
+++ SwiftUITraining/SwiftUITrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrainingApp.swift
+//  SwiftUITraining
+//
+//  Created by Son Nguyen on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
